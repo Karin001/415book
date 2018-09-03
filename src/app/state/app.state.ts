1a79cac28036b8dd96d1e54cd2079ceec8dd5a66
yuk <yukarinrin@163.com>
2018-09-03 22:48:07 +0800

    1

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,11 +1,17 @@
 import {State,Action,StateContext,Selector} from '@ngxs/store'
 import {IndexStateModel} from './app.stateModel'
-
+import {IndexLoadStart} from './app.action'
 
 @State<IndexStateModel>({
   name:'app',
   defaults:{bookList:{}}
 })
-export class {
+export class AppState{
+  constructor(){
 
+  }
+  @Action(IndexLoadStart)
+  loadIndexBookList(ctx:StateContext<IndexStateModel>,action:IndexLoadStart){
+
+  }
 }
